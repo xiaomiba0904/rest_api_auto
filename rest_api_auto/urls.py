a2b422624138cf791d1bf038193865ab6a8d6611
葛汉斌 <xiaomiba0904@gmail.com>
2019-05-21 17:28:26 +0800

    add view dispatch

--- rest_api_auto/urls.py
+++ rest_api_auto/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from rest_api_auto.views import list_or_create_view, get_or_update_view
+from rest_api_auto.views import ListCreateApiView, GetUpdateApiView
 
 
 urlpatterns = [
-    path('<str: app>/<str: model>/', list_or_create_view),
-    path('<str: app>/<str: model>/<slug: pk>/', get_or_update_view),
+    path('<str: app>/<str: model>/', ListCreateApiView()),
+    path('<str: app>/<str: model>/<slug: pk>/', GetUpdateApiView()),
 ]
--- rest_api_auto/views.py
+++ rest_api_auto/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponseNotFound
+from rest_framework import exceptions
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView
 
 from rest_api_auto import _api_globals
 from rest_api_auto.utils import url_parameter_conversion
 
 
 class DispatchView(object):
+
+    _route = {}
 
     def __call__(self, request, *args, **kwargs):
         name, _ = url_parameter_conversion(**kwargs)
@@ -14,12 +18,32 @@ class DispatchView(object):
 
         self.manager = _api_globals[name]
 
+        if request.method not in self._route.keys():
+            raise exceptions.MethodNotAllowed(request.method)
+
+        view_class = self.dispatch(request)
+        return view_class.as_view()(request, *args, **kwargs)
+
+    def dispatch(self, request):
+        view_class = self._route.get(request.method)
+        if not view_class:
+            raise exceptions.MethodNotAllowed(request.method)
+        return view_class
 
 
-def list_or_create_view(request, *args, **kwargs):
-    pass
+class ListCreateApiView(DispatchView):
+
+    _route = {
+        "GET": ListAPIView,
+        "POST": CreateAPIView
+    }
 
 
+class GetUpdateApiView(DispatchView):
 
-def get_or_update_view(request, *args, **kwargs):
-    pass
+    _route = {
+        "GET": RetrieveAPIView,
+        "PUT": UpdateAPIView,
+        "PATCH": UpdateAPIView,
+        "DELETE": DestroyAPIView
+    }
