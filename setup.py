99c7a7a3b8fff867533d03478ff001f7dbc8b78a
xiaomibaghb <xiaomiba0904@gmail.com>
2019-08-29 14:55:45 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="rest_api_auto",
+    version="1.0",
+    packages=find_packages(exclude=["tests", "example"]),
+    author="xiaomiba0904",
+    author_email="xiaomiba0904@gmail.com",
+    url="https://github.com/xiaomiba0904/rest_api_auto"
+)
