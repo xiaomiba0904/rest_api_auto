7ec95a8c9374f798014c98f009c8a3f6d0d4876b
xiaomibaghb <xiaomiba0904@gmail.com>
2019-08-30 09:33:23 +0800

    优化装饰器

--- rest_api_auto/base.py
+++ rest_api_auto/base.py
@@ -10,7 +10,6 @@ from rest_api_auto import CRUD
 
 
 class DefaultPagination(PageNumberPagination):
-    page_size = 10
 
     def get_paginated_response(self, data):
         return self.get_json_response(data)
--- rest_api_auto/decorators.py
+++ rest_api_auto/decorators.py
@@ -2,23 +2,29 @@ from rest_api_auto import _api_globals, CRUD
 from rest_api_auto.base import APIManager
 
 
-def register_model(name=None, actions=CRUD):
+def register_model(name=None, actions=None):
     """
-    model 装饰器
+    model 注册装饰器
+    :param name: app.model
+      默认为Model._meta.label
+    :param actions: 增："C"， 删："D", 改："U", 查："R"
     """
-    if actions not in CRUD:
-        raise ValueError("操作方法错误，只能使用CRUD内")
+    if actions:
+        if not set(actions).issubset(set(CRUD)):
+                raise ValueError("操作方法错误，只能使用CRUD内")
+    else:
+        actions = CRUD
 
-    if name and name in _api_globals:
-        raise ValueError("API 名称重复")
+    if name:
+        if name in _api_globals:
+            raise ValueError("API 名称重复")
+        if '.' not in name:
+            raise ValueError("name 必须有 . 符号")
 
     def wrapper(model_class):
         api_name = name or model_class._meta.label
-        if api_name in _api_globals:
-            return model_class
-
-        api_manager = APIManager(api_name, model_class, actions=actions)
-        _api_globals[api_name] = api_manager
+        if api_name not in _api_globals:
+            _api_globals[api_name] = APIManager(api_name, model_class, actions=actions)
         return model_class
 
     return wrapper
